7ec41587d594c3a35b2b8b10564d497175283fc7
Wong Wei Jun <162281012+SakaiDaBest@users.noreply.github.com>
2026-03-02 22:50:26 +0800

    Update email address in wish_gen.py

--- wish_gen.py
+++ wish_gen.py
@@ -20,7 +20,7 @@ MONTHS = {
     1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun",
     7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"
 }
-my_email = "wong.weijun923@gmail.com"
+my_email = "example@gmail.com"
 BASE_DIR = Path(__file__).resolve().parent
 JSON_PATH = BASE_DIR / "bday.json"
 
